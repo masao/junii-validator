f11a0d244acd372ac46615eaf5aa776b595052c9
Masao Takaku <tmasao@acm.org>
2010-09-16 11:15:56 +0900

    add basic tests.

---
+++ test.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+# $Id$
+
+require 'test/unit'
+
+require "validator.rb"
+
+class TestJuNII2Validator < Test::Unit::TestCase
+   def test_validator
+      [
+       "http://eprints.lib.hokudai.ac.jp/dspace-oai/request",
+       "http://repository.kulib.kyoto-u.ac.jp/dspace-oai/request",
+       "http://koara.lib.keio.ac.jp/xoonips/modules/xoonips/oai.php",
+      ].each do |url|
+         validator = JuNii2Validator.new( url )
+         STDERR.puts
+         result = validator.validate
+         result[ :error ].each do |e|
+            assert( e.kind_of?( Hash ) )
+            p e
+            assert( e[ :error_id ] )
+         end
+      end
+   end
+end
